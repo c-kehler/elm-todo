58386481b5f20ae5d435ed95d3211c7c4db10fe3
spencer <spencer@mackeycorp.com>
2019-09-09 19:47:00 -0400

    Swap increment/decrement button location

--- example/main.elm
+++ example/main.elm
@@ -41,7 +41,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
+        [ button [ onClick Increment ] [ text "+" ]
         , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Decrement ] [ text "-" ]
         ]
